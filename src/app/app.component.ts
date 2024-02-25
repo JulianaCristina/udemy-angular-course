1f8d41d532dc27e02a878e179a0a71a1e2a05bc2
Juliana Gonçalves <julianacristina780@gmail.com>
2024-02-25 20:09:14 -0300

    Style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent {
   isDisabled = false
   buttonTitle = "Texto do botão"
   buttonDisabled = false
+  buttonWidth1 = "110px"
+  buttonWidth2 = 130
+  stylesObj = {width: '160px', backgroundColor: 'grey'}
 
 
   enableInput(){
@@ -36,6 +39,22 @@ export class AppComponent {
   handleInputKeyUp(event: any){
     this.inputText = event.target.value
     console.log(event)
+  }
+
+  updateStyleObj(){
+    this.stylesObj.width = '170px';
+    this.stylesObj.backgroundColor = 'lightblue'
+  }
+
+  updateStyleObjCorrect(){
+    this.stylesObj = {
+      width: '170px',
+      backgroundColor: 'lightblue'
+    }
+  }
+
+  updateWidth(){
+    this.buttonWidth2 = 200
   }
 
 
